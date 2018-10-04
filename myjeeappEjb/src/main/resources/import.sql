649ffe3d22bb32dcd6840f58e742ae3f50ec03af
didier-defrance <didier@d-defrance.fr>
2018-10-04 16:31:44 +0200

    config jpa

---
+++ myjeeappEjb/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+//import.sql est un script SQL automatiquement déclenché par hibernate 
+//au démarrage de appli (ou test) en mode hibernate.hbm2ddl.auto=create  
+
+INSERT INTO Client(numClient,prenom,nom) VALUES (1,'alex','Therieur');
