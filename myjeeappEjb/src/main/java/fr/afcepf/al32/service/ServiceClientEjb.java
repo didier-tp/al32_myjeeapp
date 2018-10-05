0e208139cefbe9e0055b0f8508b537d205a8d6d8
didier-defrance <didier@d-defrance.fr>
2018-10-05 16:36:22 +0200

    avec commentaires dans persistence.xml

--- myjeeappEjb/src/main/java/fr/afcepf/al32/service/ServiceClientEjb.java
+++ myjeeappEjb/src/main/java/fr/afcepf/al32/service/ServiceClientEjb.java
@@ -28,7 +28,9 @@ public class ServiceClientEjb implements IServiceClient {
 
 	@Override
 	public Client saveOrUpdateClient(Client c) {
+		System.out.println("numClient="+c.getNumero());
 		clientDao.save(c);
+		System.out.println("numClient="+c.getNumero());
 		if(c.getAdressePrincipale().getCp().equals("-1")) {
 			throw new RuntimeException("-1 = codePostal invalide");
 			//exemple "scolaire" pour visualiser rollback automatique sur EJB
